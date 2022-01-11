ca01de4e19e1c96206255b87432f19936073419d
gabi <gabriella.marias@gmail.com>
2022-01-11 12:51:05 -0500

    Added fav class

---
+++ AngularPlanning/Models/Favorite.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AngularPlanning.Models
+{
+    public class Favorite
+    {
+        [Key]
+        public int FavID { get; set; }
+        public int UserID { get; set; }
+        public int EventID { get; set; }
+    }
+}
